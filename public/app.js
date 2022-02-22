bc3740374a9daf2279e7609f979e72f664a6b2ed
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2022-02-22 19:21:33 +0100

    LocalStorage added 💻

--- public/app.js
+++ public/app.js
@@ -1,10 +1,11 @@
-const elemets = {
+const elements = {
   timer: document.querySelector(".timer"),
   form: document.querySelector("form"),
   start: document.querySelector(".start-btn"),
   stop: document.querySelector(".stop-btn"),
   reset: document.querySelector(".reset-btn"),
   set: document.querySelector(".set-btn"),
+  defaultTimers: document.querySelectorAll(".default-timers"),
 };
 
 const setTimer = (hoursValue, minutesValue, secondsValue) => {
@@ -14,14 +15,16 @@ const setTimer = (hoursValue, minutesValue, secondsValue) => {
   hours < 10 ? (hours = "0" + hours) : (hours = hours);
   min < 10 ? (min = "0" + min) : (min = min);
   sec < 10 ? (sec = "0" + sec) : (sec = sec);
-  elemets.timer.innerHTML = `${hours}:${min}:${sec}`;
+  let timer = `${hours}:${min}:${sec}`;
+  elements.timer.innerHTML = timer;
+  saveTimer(timer);
 };
-elemets.start.addEventListener("click", () => {
-  let interval = setInterval(() => {
-    let hours = parseInt(elemets.timer.innerHTML.split(":")[0]);
-    let min = parseInt(elemets.timer.innerHTML.split(":")[1]);
-    let sec = parseInt(elemets.timer.innerHTML.split(":")[2]);
-    console.log(hours, min, sec);
+let interval;
+elements.start.addEventListener("click", () => {
+  interval = setInterval(() => {
+    let hours = parseInt(elements.timer.innerHTML.split(":")[0]);
+    let min = parseInt(elements.timer.innerHTML.split(":")[1]);
+    let sec = parseInt(elements.timer.innerHTML.split(":")[2]);
     sec--;
     if (sec < 0) {
       sec = 59;
@@ -38,17 +41,33 @@ elemets.start.addEventListener("click", () => {
     }
     setTimer(hours, min, sec);
   }, 1000);
-}); // TODO: start timer
-
-elemets.form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const formData = new FormData(elemets.form);
-  const hours = formData.get("hours-input");
-  console.log(hours);
-  const min = formData.get("minutes-input");
-  console.log(min);
-  const sec = formData.get("seconds-input");
-  console.log(sec);
-  setTimer(hours, min, sec);
-  elemets.form.reset();
 });
+
+elements.reset.addEventListener("click", () => {
+  clearInterval(interval);
+  setTimer(0, 0, 0);
+});
+elements.stop.addEventListener("click", () => {
+  // stop the timer
+  clearInterval(interval);
+});
+
+elements.form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const formData = new FormData(elements.form);
+  const hours = formData.get("hours-input");
+  const min = formData.get("minutes-input");
+  const sec = formData.get("seconds-input");
+  setTimer(hours, min, sec);
+  elements.form.reset();
+});
+elements.defaultTimers.forEach((timer) => {
+  timer.addEventListener("click", (e) => {
+    const value = e.target.innerText;
+    setTimer(value.split(":")[0], value.split(":")[1], value.split(":")[2]);
+  });
+});
+
+const saveTimer = (timer) => {
+  localStorage.setItem("timer", timer);
+};
